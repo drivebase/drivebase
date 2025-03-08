892dadd150881e5f5fe9508ffdc66ced29701e27
mxvsh <xencodes@icloud.com>
2025-03-08 22:15:10 +0530

    refactor: simplify database module by removing repository classes

--- shared/internal/src/database.module.ts
+++ shared/internal/src/database.module.ts
@@ -1,21 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersService } from './users/users.service';
-import { UsersRepository } from './users/users.repository';
 import { ProvidersService } from './providers/providers.service';
-import { ProvidersRepository } from './providers/providers.repository';
+import { WorkspacesService } from './workspaces/workspaces.service';
 
 @Global()
 @Module({
-  providers: [
-    PrismaService,
-    // Users
-    UsersService,
-    UsersRepository,
-    // Providers
-    ProvidersService,
-    ProvidersRepository,
-  ],
+  providers: [PrismaService, UsersService, ProvidersService, WorkspacesService],
   get exports() {
     return this.providers;
   },
