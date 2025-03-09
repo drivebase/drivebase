1068da239017bae457dd4706af9dd44fdef5a573
mxvsh <xencodes@icloud.com>
2025-03-09 19:51:28 +0530

    refactor: update controllers to use custom decorators for user and workspace context

--- apps/backend/src/api/controllers/providers.controller.ts
+++ apps/backend/src/api/controllers/providers.controller.ts
@@ -11,16 +11,17 @@ import {
 import { ProvidersService } from '@xilehq/internal/providers/providers.service';
 import { CreateProviderDto } from '@xilehq/internal/providers/dtos/create.provider.dto';
 import { UpdateProviderDto } from '@xilehq/internal/providers/dtos/update.provider.dto';
-import { WorkspaceGuard } from '@xilehq/internal/workspaces/workspace.guard';
 import { GetWorkspaceFromRequest } from '@xilehq/internal/workspaces/workspace.from.request';
 import { ProviderType, Workspace } from '@prisma/client';
+import { WorkspaceGuard } from '@xilehq/internal/workspaces/workspace.guard';
+import { CallbackProviderDto } from '@xilehq/internal/providers/dtos/callback.provider.dto';
 
-@UseGuards(WorkspaceGuard)
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
   @Post()
+  @UseGuards(WorkspaceGuard)
   async create(
     @GetWorkspaceFromRequest() workspace: Workspace,
     @Body() createProviderDto: CreateProviderDto
@@ -29,6 +30,7 @@ export class ProvidersController {
   }
 
   @Get()
+  @UseGuards(WorkspaceGuard)
   async findAll(@GetWorkspaceFromRequest() workspace: Workspace) {
     return this.providersService.findByWorkspaceId(workspace.id);
   }
@@ -49,6 +51,17 @@ export class ProvidersController {
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return this.providersService.delete(id);
+  }
+
+  @Get(':id/auth')
+  async connect(@Param('id') id: string) {
+    return this.providersService.getAuthUrl(id);
+  }
+
+  @Post('callback')
+  async callback(@Body() body: CallbackProviderDto) {
+    console.log('body', body);
+    return this.providersService.callback(body);
   }
 
   @Get('type/:type')
