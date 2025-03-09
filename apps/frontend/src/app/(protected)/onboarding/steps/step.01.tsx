e77d250971a018c1f12d82bbfa13c1fe10d6b624
mxvsh <xencodes@icloud.com>
2025-03-09 19:52:06 +0530

    feat: implement onboarding flow with multi-step workspace creation

---
+++ apps/frontend/src/app/(protected)/onboarding/steps/step.01.tsx
@@ -0,0 +1,61 @@
+import { motion } from 'framer-motion';
+import { Button } from '@xilehq/ui/components/button';
+import { Input } from '@xilehq/ui/components/input';
+import { useRef, useState } from 'react';
+import { useCreateWorkspaceMutation } from '@xilehq/ui/lib/redux/endpoints/workspaces';
+
+type StepOneProps = {
+  onNext: (workspaceId: string) => void;
+};
+
+function StepOne({ onNext }: StepOneProps) {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const [createWorkspace, { isLoading }] = useCreateWorkspaceMutation();
+  const [loading, setLoading] = useState(false);
+  const handleNext = async () => {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    onNext('cded9b17-85b3-464f-a4fc-5b31c839543d');
+    return;
+    // const name = nameRef.current?.value;
+    // if (name) {
+    //   createWorkspace({ name })
+    //     .unwrap()
+    //     .then((workspace) => {
+    //       if (workspace.data) {
+    //         onNext(workspace.data.id);
+    //       }
+    //     });
+    // }
+  };
+
+  return (
+    <div className="space-y-4">
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-sm text-muted-foreground"
+      >
+        <Input placeholder="Enter your workspace name" ref={nameRef} />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+      >
+        <Button
+          className="px-10"
+          variant={'outline'}
+          onClick={handleNext}
+          isLoading={loading}
+        >
+          Create
+        </Button>
+      </motion.div>
+    </div>
+  );
+}
+
+export default StepOne;
