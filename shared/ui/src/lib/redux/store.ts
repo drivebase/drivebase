ce4d0641cd93c70f37c44ebb9e81d189f57b4341
mxvsh <xencodes@icloud.com>
2025-03-09 19:50:56 +0530

    refactor: update Redux store configuration for new API endpoints

--- shared/ui/src/lib/redux/store.ts
+++ shared/ui/src/lib/redux/store.ts
@@ -5,15 +5,23 @@ import profileReducer from './reducers/profile.reducer';
 
 // Endpoints
 import authApi from './endpoints/auth';
+import workspaceApi from './endpoints/workspaces';
+import providersApi from './endpoints/providers';
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       profile: profileReducer,
       [authApi.reducerPath]: authApi.reducer,
+      [workspaceApi.reducerPath]: workspaceApi.reducer,
+      [providersApi.reducerPath]: providersApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(authApi.middleware),
+      getDefaultMiddleware().concat(
+        authApi.middleware,
+        workspaceApi.middleware,
+        providersApi.middleware
+      ),
   });
 };
 
