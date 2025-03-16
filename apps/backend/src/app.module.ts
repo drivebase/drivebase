dbcd0ff2a64a4d971f0a054ea60d674895fdb560
mxvsh <xencodes@icloud.com>
2025-03-16 19:37:07 +0530

    feat: add ServeStaticModule to serve frontend assets

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,18 @@ import { Global, Module } from '@nestjs/common';
 import { DatabaseModule } from '@drivebase/internal/database.module';
 import { ApiModule } from '@drivebase/backend/api/api.module';
 import { WorkspaceProvider } from '@drivebase/internal/workspaces/workspace.provider';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Global()
 @Module({
-  imports: [DatabaseModule, ApiModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend'),
+    }),
+    DatabaseModule,
+    ApiModule,
+  ],
   controllers: [],
   providers: [WorkspaceProvider],
   exports: [WorkspaceProvider],
