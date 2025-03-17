8a93c979ae4f80e82fd8017a7fd9e483d26fc931
mxvsh <xencodes@icloud.com>
2025-03-17 13:12:34 +0530

    feat: enhance sidebar with user account dropdown and version display

--- apps/frontend/src/components/layouts/app.sidebar.tsx
+++ apps/frontend/src/components/layouts/app.sidebar.tsx
@@ -18,10 +18,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@drivebase/react/components/dropdown-menu';
-import { ChevronLeftIcon } from 'lucide-react';
-import { useLocation, Link } from '@tanstack/react-router';
+import { ChevronLeftIcon, ChevronsUpDown } from 'lucide-react';
+import { useLocation, Link, useRouteContext } from '@tanstack/react-router';
 import { Avatar, AvatarFallback } from '@drivebase/react/components/avatar';
-import { useAppSelector } from '@drivebase/react/lib/redux/hooks';
 import {
   mainItems,
   settingsItems,
@@ -30,14 +29,14 @@ import SidebarUpload from './upload';
 
 const AppSidebar = () => {
   const location = useLocation();
-  const profile = useAppSelector((s) => s.profile.user);
+  const context = useRouteContext({ from: '/_protected' });
 
   const showSettings = location.pathname.startsWith('/settings');
   const currentItems = showSettings ? settingsItems : mainItems;
 
   return (
     <Sidebar className="border-transparent w-[15rem]">
-      <SidebarHeader className="z-10 space-y-4 pt-4 justify-between">
+      <SidebarHeader className="z-10 space-y-4 justify-between">
         <div className="flex justify-between items-start">
           <img
             draggable={false}
@@ -47,23 +46,7 @@ const AppSidebar = () => {
             width={40}
             className="rounded-xl"
           />
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Avatar className="w-8 h-8 select-none">
-                <AvatarFallback className="bg-accent-foreground text-accent">
-                  {profile?.name?.charAt(0)}
-                </AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <span className="text-xs font-medium">{context.version}</span>
         </div>
         <SidebarUpload />
       </SidebarHeader>
@@ -99,7 +82,48 @@ const AppSidebar = () => {
         </SidebarGroup>
       </SidebarContent>
 
-      <SidebarFooter className="z-10" />
+      <SidebarFooter className="z-10 border-t">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-primary-foreground"
+            >
+              <div className="flex aspect-square size-8 items-center justify-center">
+                <Avatar className="w-8 h-8 select-none">
+                  <AvatarFallback className="bg-primary text-accent">
+                    {context.auth.user?.name?.charAt(0)}
+                  </AvatarFallback>
+                </Avatar>
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">
+                  {context.auth.user?.name}
+                </span>
+                <span className="truncate text-xs">
+                  {context.auth.user?.email}
+                </span>
+              </div>
+              <ChevronsUpDown className="ml-auto" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[200px]">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link to="/settings">Settings</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => {
+                context.auth.logout();
+              }}
+            >
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarFooter>
 
       <div className="absolute inset-0 z-0 overflow-hidden">
         <div className="pointer-events-none absolute -left-2/3 bottom-0 aspect-square w-[140%] translate-y-1/4 rounded-full bg-[conic-gradient(from_32deg_at_center,#855AFC_0deg,#3A8BFD_72deg,#00FFF9_144deg,#5CFF80_198deg,#EAB308_261deg,#f00_360deg)] opacity-15 blur-[75px]" />
