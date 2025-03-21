2369bd396743f5ee99f72e1dda8e5d3623a1dc5a
mxvsh <xencodes@icloud.com>
2025-03-21 22:26:06 +0530

    refactor: reorder database migration execution in entrypoint script

--- var/docker/entrypoint.sh
+++ var/docker/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# Run database migrations
-echo "Running database migrations..."
-node scripts/migrate-deploy.mjs
-
 if [ "$1" = "telegram-login" ]; then
   echo "Running Telegram login..."
   node scripts/telegram-login.mjs
   exit 0
 fi
+
+# Run database migrations
+echo "Running database migrations..."
+node scripts/migrate-deploy.mjs
+
 
 /usr/bin/supervisord -c /etc/supervisord.conf
