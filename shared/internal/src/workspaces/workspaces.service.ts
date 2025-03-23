c94f37a8a1996e3e82b40da41622833c40d4a310
ajstudd <j7654894110@gmail.com>
2025-03-23 20:10:39 +0530

    feat(workspaces): implement WorkspaceStatDto and update getWorkspaceStats method

--- shared/internal/src/workspaces/workspaces.service.ts
+++ shared/internal/src/workspaces/workspaces.service.ts
@@ -3,7 +3,7 @@ import { Workspace } from '@prisma/client';
 import { PrismaService } from '@drivebase/internal/prisma.service';
 import { CreateWorkspaceDto } from './dtos/create.workspace.dto';
 import { UpdateWorkspaceDto } from './dtos/update.workspace.dto';
-import { WorkspaceStats } from '../types/workspace.types';
+import { WorkspaceStatDto } from './dtos/stats.workspace.dto';
 
 @Injectable()
 export class WorkspacesService {
@@ -62,7 +62,7 @@ export class WorkspacesService {
     });
   }
 
-  async getWorkspaceStats(id: string): Promise<WorkspaceStats> {
+  async getWorkspaceStats(id: string): Promise<WorkspaceStatDto[]> {
     const files = await this.prisma.file.findMany({
       where: {
         workspaceId: id,
@@ -81,20 +81,29 @@ export class WorkspacesService {
     };
 
     for (const file of files) {
+      if (!file.mimeType) {
+        continue;
+      }
+      const size = file.size || 0;
       if (file.mimeType.startsWith('image')) {
-        stats.image.size += file.size;
+        stats.image.size += size;
         stats.image.count += 1;
       } else if (file.mimeType.startsWith('video')) {
-        stats.video.size += file.size;
+        stats.video.size += size;
         stats.video.count += 1;
       } else if (file.mimeType.startsWith('application')) {
-        stats.application.size += file.size;
+        stats.application.size += size;
         stats.application.count += 1;
       } else {
-        stats.others.size += file.size;
+        stats.others.size += size;
         stats.others.count += 1;
       }
     }
-    return stats;
+    return [
+      { title: 'Images', ...stats.image },
+      { title: 'Videos', ...stats.video },
+      { title: 'Documents', ...stats.application },
+      { title: 'Others', ...stats.others },
+    ];
   }
 }
