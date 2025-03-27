e261ceaf33ae7116c2922b55b2cd44f818f45146
mxvsh <xencodes@icloud.com>
2025-03-27 19:43:53 +0530

    feat(database): integrate TypeORM with entity models in db.module.ts
    - Added TypeOrmModule import to db.module.ts for database entity management.
    - Included entity models for User, Workspace, Provider, and File to streamline database interactions.
    - Enhanced main.ts by importing reflect-metadata for TypeORM compatibility.

--- libs/database/src/db.module.ts
+++ libs/database/src/db.module.ts
@@ -1,13 +1,21 @@
 import { FilesService } from '@drivebase/files';
+import { File } from '@drivebase/files/file.entity';
 import { ProvidersService } from '@drivebase/providers';
+import { Provider } from '@drivebase/providers/provider.entity';
 import { UsersService } from '@drivebase/users';
+import { User } from '@drivebase/users/user.entity';
 import { WorkspacesService } from '@drivebase/workspaces';
+import { Workspace } from '@drivebase/workspaces/workspace.entity';
 import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PrismaService } from './prisma.service';
 
+export const entities = [User, Workspace, Provider, File];
+
 @Global()
 @Module({
+  imports: [TypeOrmModule.forFeature(entities)],
   providers: [
     PrismaService,
     UsersService,
