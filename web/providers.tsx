992826c4c4c3b035daf931ebcee322016e00d819
mxvsh <xencodes@icloud.com>
2025-03-28 02:41:42 +0530

    feat(provider): integrate Apollo Client for GraphQL support in Providers component

--- web/providers.tsx
+++ web/providers.tsx
@@ -1,9 +1,12 @@
 import '@drivebase/web/globals.css';
 
-import { Provider as ReduxProvider } from 'react-redux';
-import { ThemeProvider } from './theme.provider';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { makeStore } from '@drivebase/web/lib/redux/store';
+import { Provider as ReduxProvider } from 'react-redux';
 import { Toaster } from 'sonner';
+
+import { config } from './constants/config';
+import { ThemeProvider } from './theme.provider';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -11,13 +14,20 @@ interface ProvidersProps {
 
 const store = makeStore();
 
+const client = new ApolloClient({
+  uri: `${config.apiUrl}/graphql`,
+  cache: new InMemoryCache(),
+});
+
 export function Providers({ children }: ProvidersProps) {
   return (
-    <ThemeProvider defaultTheme="system">
-      <ReduxProvider store={store}>
-        <Toaster position="bottom-center" />
-        {children}
-      </ReduxProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider defaultTheme="system">
+        <ReduxProvider store={store}>
+          <Toaster position="bottom-center" />
+          {children}
+        </ReduxProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
