d40fc1d93bf49b48c3a326d6411f5d78ec3b3d66
mxvsh <xencodes@icloud.com>
2025-03-31 00:22:54 +0530

    fix(_protected.tsx): enhance error handling for Apollo queries by distinguishing between fetch and authorization errors

--- web/routes/_protected.tsx
+++ web/routes/_protected.tsx
@@ -1,5 +1,5 @@
-import { useQuery } from '@apollo/client';
-import { Outlet, createFileRoute, useRouter } from '@tanstack/react-router';
+import { ApolloError, useQuery } from '@apollo/client';
+import { Navigate, Outlet, createFileRoute, useRouter } from '@tanstack/react-router';
 import { Loader } from 'lucide-react';
 import { SignalIcon } from 'lucide-react';
 import { useEffect } from 'react';
@@ -27,7 +27,7 @@ function RouteComponent() {
 
   useEffect(() => {
     if (error && 'status' in error && error.status === 401) {
-      router.navigate({ to: '/auth/login' });
+      void router.navigate({ to: '/auth/login' });
     }
   }, [error, router]);
 
@@ -41,23 +41,31 @@ function RouteComponent() {
 
   if (!error) return <Outlet />;
 
-  if ('status' in error && error.status === 'FETCH_ERROR') {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="absolute inset-0 h-full w-full bg-background bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
-        <Card className="w-full max-w-sm shadow-xl z-10 rounded-2xl relative">
-          <CardHeader className="border-b text-center py-12">
-            <SignalIcon className="w-20 h-20 mx-auto mb-4 p-4 bg-muted rounded-xl" />
-            <CardTitle className="text-xl font-medium">{t('errors:not_reachable_title')}</CardTitle>
-            <CardDescription>{t('errors:not_reachable_description')}</CardDescription>
-          </CardHeader>
-          <CardContent className="pt-8 space-y-4 bg-accent/50">
-            <Button variant={'outline'} onClick={() => window.location.reload()}>
-              {t('common:retry')}
-            </Button>
-          </CardContent>
-        </Card>
-      </div>
-    );
+  if (error instanceof ApolloError) {
+    if (error.message === 'Failed to fetch') {
+      return (
+        <div className="flex items-center justify-center h-screen">
+          <div className="absolute inset-0 h-full w-full bg-background bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+          <Card className="w-full max-w-sm shadow-xl z-10 rounded-2xl relative">
+            <CardHeader className="border-b text-center py-12">
+              <SignalIcon className="w-20 h-20 mx-auto mb-4 p-4 bg-muted rounded-xl" />
+              <CardTitle className="text-xl font-medium">
+                {t('errors:not_reachable_title')}
+              </CardTitle>
+              <CardDescription>{t('errors:not_reachable_description')}</CardDescription>
+            </CardHeader>
+            <CardContent className="pt-8 space-y-4 bg-accent/50">
+              <Button variant={'outline'} onClick={() => window.location.reload()}>
+                {t('common:retry')}
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      );
+    }
+
+    if (error.message === 'Unauthorized') {
+      return <Navigate to="/auth/login" />;
+    }
   }
 }
