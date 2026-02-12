86bfbf0c9262fa1d006d0e0210f20982a9195dad
mxvsh <hello@monawwar.io>
2026-02-13 00:33:36 +0530

    chore: remove Google Analytics in non-production

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -66,7 +66,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
-      <GoogleAnalytics gaId="G-60H49RJF0W" />
+      {process.env.NODE_ENV === "production" && <GoogleAnalytics gaId="G-60H49RJF0W" />}
     </html>
   );
 }
