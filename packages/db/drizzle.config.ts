5e210c6c6e936591e36360097fdb798e28374a83
mxvsh <hello@monawwar.io>
2026-02-13 13:38:41 +0530

    chore: update dotenv configuration path in drizzle.config.ts

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,8 +2,8 @@ import { joinPath } from "@drivebase/core";
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-const API_PATH = joinPath("../../apps/api/.env");
-config({ path: API_PATH });
+const ENV_PATH = joinPath("../../.env.local");
+config({ path: ENV_PATH });
 
 export default defineConfig({
 	schema: "./schema/index.ts",
