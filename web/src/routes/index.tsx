6528b4214c175a43feafc1e7e5a14ed6c5af82be
mxvsh <hello@monawwar.io>
2026-04-14 18:34:22 +0530

    Add web

---
+++ web/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@heroui/react';
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  return (
+    <>
+      <Button>HELLO</Button>
+    </>
+  )
+}
