c3f0d74b0713f7e26b70497bb5868ee54484b83d
mxvsh <hello@monawwar.io>
2026-04-25 18:54:04 +0530

    fix: export ALLOWED_EVENT_NAMES + correct ratelimits field in wrangler

--- packages/telemetry/index.ts
+++ packages/telemetry/index.ts
@@ -1,3 +1,4 @@
-export type { TelemetryEvent } from './events'
+export type { TelemetryEvent, EventName } from './events'
+export { ALLOWED_EVENT_NAMES } from './events'
 export type { TelemetryClient, TelemetryClientOptions } from './client'
 export { createTelemetryClient } from './client'
